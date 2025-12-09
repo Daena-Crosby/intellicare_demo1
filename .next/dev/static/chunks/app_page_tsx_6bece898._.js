f6632d1e8716cfb3f45276e1cc50b02f412ed916
Khrisean Stewart <157300952+KhriseanStewart@users.noreply.github.com>
2025-12-09 11:06:39 -0500

    Update to home page

--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_c71a6d65._.js",
-  "static/chunks/node_modules_61dce6ed._.js",
-  "static/chunks/app_offer-carousel_bb94c769.css"
-],
-    source: "dynamic"
-});
