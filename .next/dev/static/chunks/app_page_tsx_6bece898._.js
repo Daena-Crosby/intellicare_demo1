006088f2589d2c929a0b9a6cb73395be35c67bc9
Khrisean Stewart <157300952+KhriseanStewart@users.noreply.github.com>
2025-12-08 19:36:32 -0500

    Hello world

--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++ .next/dev/static/chunks/app_page_tsx_6bece898._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_3ae6f597._.js",
+  "static/chunks/_c71a6d65._.js",
   "static/chunks/node_modules_61dce6ed._.js",
   "static/chunks/app_offer-carousel_bb94c769.css"
 ],
